7f66033203a3137442d2352a3b6ea4446edcd765
Samuel Kjelde <kjeldes@my.lanecc.edu>
2024-11-21 22:49:02 -0800

    Got the state interface working real good!

--- Client/state/js/state.js
+++ Client/state/js/state.js
@@ -2,7 +2,7 @@ class StatePage {
     constructor() {
         this.state = {
             stateCode: "",
-            stateName: ""
+            stateName: "",
         };
 
         // instance variables that the app needs but are not part of the "state" of the application
@@ -33,8 +33,8 @@ class StatePage {
         this.onEditState = this.onEditState.bind(this);
         this.onCancel = this.onCancel.bind(this);
         this.onDeleteState = this.onDeleteState.bind(this);
-        //this.onSaveState = this.onSaveState.bind(this);
-        //this.onAddState = this.onAddState.bind(this);
+        this.onSaveState = this.onSaveState.bind(this);
+        this.onAddState = this.onAddState.bind(this);
 
         this.makeFieldsReadOnly = this.makeFieldsReadOnly.bind(this);
         this.makeFieldsRequired = this.makeFieldsRequired.bind(this);
@@ -72,34 +72,99 @@ class StatePage {
         }
     }
 
-    // makes a delete request to /api/customer/# where # is the primary key of the state
+    // makes a delete request to /api/state/# where # is the primary key of the state
     // deletes the state displayed on the screen from the database
     onDeleteState(event) {
         event.preventDefault();
-        if (this.state.stateCode != "") {
-            fetch(`${this.url}/${this.state.stateCode}`, { method: 'DELETE' })
-                .then(response => response.json())
-                .then(data => {
-                    // returns the record that we deleted so the state code should be the same
-                    if (this.state.stateCode = data.stateCode) {
-                        this.state.stateCode = "";
-                        this.$stateCode.value = "";
-                        this.clearStateFields();
-                        this.enableButtons("pageLoad");
-                        alert("State was deleted");
-                    }
-                    else {
-                        alert('There was a problem deleting state info!');
-                    }
-                })
-                .catch(error => {
+        if (this.state.stateCode == "") {
+            alert('There was a problem deleting state info, enter a state code!');
+            return;
+        }
+        fetch(`${this.url}/${this.state.stateCode}`, { method: 'DELETE' })
+            .then(response => {
+                if (response.ok) {
+                    this.state.stateCode = "";
+                    this.$stateCode.value = "";
+                    this.clearStateFields();
+                    this.enableButtons("pageLoad");
+                    alert("State was deleted");
+                }
+                else {
                     alert('There was a problem deleting state info!');
-                })
-        }
-        else {
-            // this should never happen if the right buttons are enabled
-        }
+                }
+            })
+            .catch(error => {
+                alert('There was a problem deleting state info!');
+            });
     }
+
+    // makes either a post or a put request to /api/states
+    // either adds a new state or updates an existing state in the database
+    // based on the state information in the form
+    onSaveState(event) {
+        event.preventDefault();
+        // ---
+        const stateCode = this.$stateCode.value;
+        const stateName = this.$stateName.value;
+        const requestBody = { stateCode, stateName };
+        // ---
+        fetch(`${this.url}/${stateCode}`)
+            .then(response => {
+                // Add
+                if (response.status == 404) {
+                    fetch(`${this.url}`, {
+                        method: 'POST',
+                        body: JSON.stringify(requestBody),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            if (data.stateCode) {
+                                this.state.stateCode = data.stateCode;
+                                this.state.stateName = data.stateName;
+                                this.fillStateFields();
+                                this.$stateCode.readOnly = false;
+                                this.enableButtons("found");
+                                alert("State was added.")
+                            }
+                            else {
+                                alert('There was a problem adding state info!');
+                            }
+                        })
+                        .catch(error => {
+                            alert('There was a problem adding state info!');
+                        })
+                } else {
+                    // Update
+                    // the format of the body has to match the original object exactly 
+                    // so make a copy of it and copy the values from the form
+                    fetch(`${this.url}/${stateCode}`, {
+                        method: 'PUT',
+                        body: JSON.stringify(requestBody),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    })
+                        .then(response => {
+                            if (response.ok) {
+                                this.fillStateFields();
+                                this.$stateCode.readOnly = false;
+                                this.enableButtons("found");
+                                alert("State was updated.")
+                            }
+                            else {
+                                alert('There was a problem updating state info!');
+                            }
+                        })
+                }
+            })
+            .catch(error => {
+                alert('There was a problem updating state info!');
+            })
+    }
+
 
     // makes the fields editable
     onEditState(event) {
@@ -114,9 +179,6 @@ class StatePage {
     // clears the form for input of a new customer
     onAddState(event) {
         event.preventDefault();
-        // can't edit the customer id
-        this.$stateCode.value = "";
-        this.$stateCode.readOnly = true;
         this.clearStateFields();
         this.makeFieldsReadOnly(false);
         this.makeFieldsRequired(true);
